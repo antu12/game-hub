a7383b463dd64358fe531fbc3403ace64d9d34d2
Arshad Hossain <arshad.antu@gmail.com>
2023-07-30 16:16:05 +0600

    clean up gae cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,8 +12,7 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           {game.parent_platforms && (
             <PlatformIconList
               platforms={game.parent_platforms.map((p) => p.platform)}
@@ -21,6 +20,7 @@ const GameCard = ({ game }: Props) => {
           )}
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
