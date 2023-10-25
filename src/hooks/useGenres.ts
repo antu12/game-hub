536cf826abf777ae39036442030d9500be4935a8
Arshad Hossain <arshad.antu@gmail.com>
2023-10-25 11:40:58 +0600

    refactoring fetch games with React Query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,6 @@
 import genres from "../data/genres";
+import { useQuery } from "@tanstack/react-query";
+import apiClient, { FetchResponse } from "../services/api-client";
 // import useData from "./useData";
 
 export interface Genre {
@@ -7,7 +9,15 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null });
+// const useGenres = () => ({ data: genres, isLoading: false, error: null });
 // const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => {
+  return useQuery<FetchResponse<Genre>, Error>({
+    queryKey: ["genres"],
+    queryFn: () => apiClient.get("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: genres.length, results: genres },
+  });
+};
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,17 @@
-import useData from "./useData";
+import apiClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./useGames";
+import { useQuery } from "@tanstack/react-query";
+import platforms from "../data/platforms";
+// const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () => {
+  return useQuery<FetchResponse<Platform>, Error>({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient.get("/platforms/lists/parents").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: platforms.length, results: platforms },
+  });
+};
 
 export default usePlatforms;
