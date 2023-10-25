3ccd7e4a5e04417986467495ea2ff4c18931e511
Arshad Hossain <arshad.antu@gmail.com>
2023-10-25 18:43:41 +0600

    Refactor: Simplyfy GameQuery

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -13,9 +13,9 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
-const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenreId }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -39,7 +39,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
                 onClick={() => onSelectGenre(genre)}
                 variant="link"
                 fontSize="lg"
-                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                fontWeight={genre.id === selectedGenreId ? "bold" : "normal"}
                 textAlign="left"
                 whiteSpace="normal"
               >
