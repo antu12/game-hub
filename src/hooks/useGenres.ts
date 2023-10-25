99c10aacf6ef3d4c1fef8a18c5db3a5ba1de7c7a
Arshad Hossain <arshad.antu@gmail.com>
2023-10-25 19:24:04 +0600

    Refactor: Simplyfy time calculations

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,6 @@
-import genres from "../data/genres";
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
+import genres from "../data/genres";
 import APIClient, { FetchResponse } from "../services/api-client";
 // import useData from "./useData";
 
@@ -16,7 +17,7 @@ const useGenres = () => {
   return useQuery<FetchResponse<Genre>, Error>({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24h
+    staleTime: ms("24h"),
     initialData: genres,
   });
 };
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
-import APIClient, { FetchResponse } from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import platforms from "../data/platforms";
+import APIClient, { FetchResponse } from "../services/api-client";
 // const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 
 export interface Platform {
@@ -15,7 +16,7 @@ const usePlatforms = () => {
   return useQuery<FetchResponse<Platform>, Error>({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24h
+    staleTime: ms("24h"),
     initialData: platforms,
   });
 };
